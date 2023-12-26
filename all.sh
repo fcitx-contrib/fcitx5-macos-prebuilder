88b466bc4b7558d80242ece0825e435b1132f8b1
Qijia Liu <liumeo@pku.edu.cn>
2023-12-25 22:57:26 -0500

    fix marisa.pc on arm (#2)

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
 set -e
 scripts/marisa.sh $1
-. ./common.sh $1 # let it set ARCH
-tar cjvf local-$ARCH.tar.bz2 -C build/usr/local .
+. ./common.sh $1 # let it set ARCH and HOMEBREW_PREFIX
+tar cjvf local-$ARCH.tar.bz2 -C build/$HOMEBREW_PREFIX .
