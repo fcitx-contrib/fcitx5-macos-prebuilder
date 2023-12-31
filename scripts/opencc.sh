baea41a8919185b7e1004d5049bd3ded0b030a44
ksqsf <i@ksqsf.moe>
2024-01-01 01:06:32 +0800

    use prebuilt marisa-trie

--- scripts/opencc.sh
+++ scripts/opencc.sh
@@ -2,11 +2,22 @@ set -e
 . ./common.sh $1
 cd opencc
 
-ARGS=(-DBUILD_SHARED_LIBS=OFF)
+ARGS=(
+    -DBUILD_SHARED_LIBS:BOOL=OFF
+    -DUSE_SYSTEM_MARISA:BOOL=ON
+)
 
 if [[ $ARCH != `uname -m` ]]; then
     sed -i '' 's/add_subdirectory(data)//' CMakeLists.txt
 fi
+
+# Use prebuilt marisa-trie
+export CMAKE_FIND_ROOT_PATH=$ROOT/build/marisa/$HOMEBREW_PREFIX
+cd src
+cat >> CMakeLists.txt <<EOF
+include_directories($ROOT/build/marisa/$HOMEBREW_PREFIX/include)
+EOF
+cd -
 
 f5m_configure ${ARGS[@]}
 f5m_build
