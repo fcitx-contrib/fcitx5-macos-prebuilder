572479ecaa60315453f3cd9410df8766a9421636
Qijia Liu <liumeo@pku.edu.cn>
2023-12-31 20:49:44 -0500

    add rime plugins submodule

--- scripts/librime.sh
+++ scripts/librime.sh
@@ -3,23 +3,21 @@ set -e
 cd librime
 
 # Enable plugins
-rime_plugins=(hchunhui/librime-lua lotem/librime-octagram)
+rime_plugins=(lua octagram)
+pushd plugins
 for plugin in ${rime_plugins[@]}; do
-    bash install-plugins.sh $plugin
+  [[ -L "$plugin" ]] || ln -s ../../librime-$plugin $plugin
 done
+popd
 
 # Work around an upstream mistake. (hchunhui/librime-lua#300)
 (cd plugins/lua; sed -i '' 's/LUA_LIBRARIES/LUA_LDFLAGS/' CMakeLists.txt)
 
-# HACK: Force generation of rime.pc
-sed -i '' 's/MINGW/APPLE/' CMakeLists.txt
-
 # Build librime
 ARGS=(
-    -DBUILD_TEST:BOOL=OFF
-    -DBUILD_MERGED_PLUGINS:BOOL=ON
-    -DENABLE_EXTERNAL_PLUGINS:BOOL=OFF
-    "-DCMAKE_PREFIX_PATH=$ROOT/build/marisa/$HOMEBREW_PREFIX"
+  -DBUILD_TEST:BOOL=OFF
+  -DBUILD_MERGED_PLUGINS:BOOL=ON
+  -DENABLE_EXTERNAL_PLUGINS:BOOL=OFF
 )
 f5m_configure ${ARGS[@]}
 f5m_build
