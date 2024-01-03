a55bcbff746d03b8f72b68665b22cd2f6744079f
ksqsf <login@ksqsf.moe>
2024-01-03 10:43:01 +0800

    prebuild opencc (#7)
    Co-authored-by: Qijia Liu <liumeo@pku.edu.cn>

---
+++ scripts/opencc.sh
@@ -0,0 +1,18 @@
+set -e
+. ./common.sh $1
+cd opencc
+
+ARGS=(
+  -DCMAKE_CXX_FLAGS="-I$HOMEBREW_PREFIX/include"
+  -DUSE_SYSTEM_MARISA:BOOL=ON
+)
+
+if [[ $ARCH != `uname -m` ]]; then
+  git reset --hard
+  sed -i '' 's/add_subdirectory(data)//' CMakeLists.txt
+fi
+
+f5m_configure ${ARGS[@]}
+f5m_build
+f5m_install opencc
+f5m_make_tarball opencc
