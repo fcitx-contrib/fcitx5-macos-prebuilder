12d60e8e97dfdf0427498a2f3c63fed848fd07ed
Qijia Liu <liumeo@pku.edu.cn>
2024-01-06 17:21:36 -0500

    add lua.pc and fix include dir

--- scripts/lua.sh
+++ scripts/lua.sh
@@ -7,10 +7,13 @@ make a CFLAGS="-O3 -arch ${ARCH}"
 
 include_dir=$ROOT/build/lua$INSTALL_PREFIX/include
 lib_dir=$ROOT/build/lua$INSTALL_PREFIX/lib
-mkdir -p $include_dir $lib_dir
+mkdir -p $include_dir/lua $lib_dir/pkgconfig
 
-cp lua.h luaconf.h lualib.h lauxlib.h $include_dir
-cp $ROOT/patches/lua.hpp $include_dir
+cp lua.h luaconf.h lualib.h lauxlib.h $ROOT/patches/lua.hpp $include_dir/lua
 cp liblua.a $lib_dir
+cp $ROOT/patches/lua.pc $lib_dir/pkgconfig
+
+mkdir -p $INSTALL_PREFIX
+cp -r $include_dir $lib_dir $INSTALL_PREFIX
 
 f5m_make_tarball lua
