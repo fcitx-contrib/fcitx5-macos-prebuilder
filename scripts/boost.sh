e4c36580c1fea0dd8c33b732399abbbd89fb1bed
Qijia Liu <liumeo@pku.edu.cn>
2024-01-05 21:50:02 -0500

    headers; disable migrators

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -11,7 +11,7 @@ boost_tar="$boost_dir.tar.xz"
 cd $boost_dir
 
 ARGS=(
-  -DBOOST_INCLUDE_LIBRARIES=iostreams
+  -DBOOST_INCLUDE_LIBRARIES="algorithm;bimap;container;iostreams;multi_index;ptr_container"
 )
 
 f5m_configure "${ARGS[@]}"
