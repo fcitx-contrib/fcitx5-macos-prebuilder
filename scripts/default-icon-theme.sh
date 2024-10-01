dc8a5ebdc9f6f81ab916ced75dd7e2496929f49f
Qijia Liu <liumeo@pku.edu.cn>
2024-09-30 20:46:57 -0400

    use meson for default-icon-theme

--- scripts/default-icon-theme.sh
+++ scripts/default-icon-theme.sh
@@ -2,9 +2,15 @@ set -e
 . ./common.sh $1
 cd default-icon-theme
 
+SETUP_ARGS=(
+  --buildtype=release
+  --prefix=$INSTALL_PREFIX
+)
+
 # 'Hicolor' the default icon theme
-autoreconf -i
-./configure --prefix="$INSTALL_PREFIX"
-make
-DESTDIR="$ROOT/build/default-icon-theme" make install
+meson setup build "${SETUP_ARGS[@]}"
+ninja -C build clean
+ninja -C build
+ninja -C build install
+DESTDIR=$ROOT/build/default-icon-theme ninja -C build install
 f5m_make_tarball default-icon-theme
